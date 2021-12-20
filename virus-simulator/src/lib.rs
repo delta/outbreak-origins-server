4184e0a1f479a4cee307ad349cd0af2cb2d768ab
Shreyas Krishnakumar <shreyaskumar1035@gmail.com>
2021-12-21 00:54:37 +0530

    fix(Simulator): Make solver state use normal float
    Solver state need not use a vector of refs to the values, it can use a
    vector of floats.

--- virus-simulator/src/lib.rs
+++ virus-simulator/src/lib.rs
@@ -1,16 +1,18 @@
+#![crate_name = "virus_simulator"]
 use ode_solvers::dopri5::*;
 use ode_solvers::*;
 
-type State<'a> = Vector5<&'a f64>;
+type State = Vector5<f64>;
 type Time = f64;
 
+/// Simulator based on SEIR Model
 pub struct Simulator<'a> {
     susceptible: &'a f64,
     exposed: &'a f64,
     infectious: &'a f64,
     removed: &'a f64,
-    ideal_reproduction_number: &'a f64,
     current_reproduction_number: &'a f64,
+    ideal_reproduction_number: &'a f64,
     compliance_factor: &'a f64,
     recovery_rate: &'a f64,
     infection_rate: &'a f64,
@@ -22,8 +24,8 @@ impl<'a> Simulator<'a> {
         exposed: &'a f64,
         infectious: &'a f64,
         removed: &'a f64,
-        ideal_reproduction_number: &'a f64,
         current_reproduction_number: &'a f64,
+        ideal_reproduction_number: &'a f64,
         compliance_factor: &'a f64,
         recovery_rate: &'a f64,
         infection_rate: &'a f64,
@@ -33,30 +35,24 @@ impl<'a> Simulator<'a> {
             exposed,
             infectious,
             removed,
-            ideal_reproduction_number,
             current_reproduction_number,
+            ideal_reproduction_number,
             compliance_factor,
             recovery_rate,
             infection_rate,
         }
     }
-
 }
 
-impl<'a> ode_solvers::System<State<'a>> for Simulator<'a> {
+impl<'a> ode_solvers::System<State> for Simulator<'a> {
     fn system(&self, _t: Time, y: &State, dy: &mut State) {
-        let self.susceptible = y[0];
-        let self.exposed = y[1];
-        let self.infectious = y[2];
-        let self.removed = y[3];
-        let self.current_reproduction_number = y[4];
+        // y[0..4] represent S, E, I, R and current reproduction number respectively
 
-        dy[0] = -self.recovery_rate * current_reproduction_number * susceptible * infectious;
-        dy[1] = self.recovery_rate * current_reproduction_number * susceptible * infectious
-            - self.infection_rate * exposed;
-        dy[2] = self.infection_rate * self.exposed - self.recovery_rate * infectious;
-        dy[3] = self.recovery_rate * self.infectious;
-        dy[4] = self.social_parameter * (self.ideal_reproduction_number - self.reproduction_number);
+        dy[0] = -self.recovery_rate * y[4] * y[0] * y[2];
+        dy[1] = self.recovery_rate * y[4] * y[0] * y[2] - self.infection_rate * y[1];
+        dy[2] = self.infection_rate * y[1] - self.recovery_rate * y[2];
+        dy[3] = self.recovery_rate * y[2];
+        dy[4] = self.compliance_factor * (self.ideal_reproduction_number - y[4]);
     }
 }
 
