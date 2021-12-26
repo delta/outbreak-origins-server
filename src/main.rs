3c8ad9e752fe91e4ddf2bf58f6827953e0ecfa62
Raghul Nanth <nanth.raghul@gmail.com>
2021-12-26 12:20:49 +0530

    feat: Better module separation

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,16 @@
 #[macro_use]
 extern crate diesel;
 
-use actix_cors::Cors;
-use actix_web::{http, App, HttpServer};
+use actix_web::{App, HttpServer};
 use diesel::prelude::*;
 use diesel::r2d2::{self, ConnectionManager};
 use dotenv::dotenv;
-use serde_json;
 
 mod actions;
 mod models;
-mod routes;
 mod schema;
 mod utils;
+mod auth;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -25,30 +23,11 @@ async fn main() -> std::io::Result<()> {
         .expect("Failed to create pool");
     let bind = "127.0.0.1:8081";
     HttpServer::new(move || {
-        let cors = Cors::default()
-            .allowed_origin("http://localhost:8080")
-            .allowed_methods(vec!["POST"])
-            .allowed_headers(vec![
-                http::header::CONTENT_TYPE,
-                http::header::ACCESS_CONTROL_ALLOW_HEADERS,
-                http::header::ACCESS_CONTROL_ALLOW_ORIGIN,
-                http::header::ACCESS_CONTROL_ALLOW_CREDENTIALS,
-            ])
-            // .expose_headers(vec![
-            //     http::header::SET_COOKIE,
-            //     http::header::ACCESS_CONTROL_ALLOW_CREDENTIALS,
-            // ])
-            .supports_credentials();
-        // let cors = Cors::permissive();
         App::new()
             // set up DB pool to be used with web::Data<Pool> extractor
             .data(pool.clone())
             // .wrap(middleware::Logger::default())
-            .service(routes::get_user)
-            .wrap(cors)
-            .service(routes::register_user)
-            .service(routes::login_user)
-        // .service(routes::google_auth)
+            .configure(auth::routes::auth_routes)
     })
     .bind(&bind)?
     .run()
--- src/models.rs
+++ src/models.rs
@@ -1,7 +1,7 @@
-mod auth;
 mod jwt;
+mod types;
 mod user;
 
-pub use auth::AuthResult;
 pub use jwt::{Claims, Error, Result};
+pub use types::DbPool;
 pub use user::{NewUser, User};
