f86204fd98916a7d9e4d2f2dd745dccf8914ec97
KarthikSundar2002 <karthiksundar30092002@gmail.com>
2022-01-04 15:09:02 +0530

    refactor: creates events module in db
    Moves all the code responsible for the possible actions on the events table to a separate file

--- src/db.rs
+++ src/db.rs
@@ -23,9 +23,9 @@ pub fn create_db_pool() -> PgPool {
         .expect("Failed to create pool")
 }
 
-pub fn find_event_by_id(conn: &PgConnection, id: i32) -> Result<Option<models::Event>, DbError> {
-    use crate::db::schema::events::dsl::*;
+// pub fn find_event_by_id(conn: &PgConnection, id: i32) -> Result<Option<models::Event>, DbError> {
+//     use crate::db::schema::events::dsl::*;
 
-    let event_res = events.find(id).first(conn).optional()?;
-    Ok(event_res)
-}
+//     let event_res = events.find(id).first(conn).optional()?;
+//     Ok(event_res)
+// }
