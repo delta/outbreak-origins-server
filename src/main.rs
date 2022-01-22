00b0476d1bc29e926b20cee5f2e7e4ab1f24dc1d
Shreyas Krishnakumar <shreyaskumar1035@gmail.com>
2022-01-23 01:29:51 +0530

    feat(tests): Add setup for integration testing

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,69 @@ use actix_files as fs;
 use actix_identity::IdentityService;
 use actix_web::middleware as mw;
 use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer};
+use diesel::PgConnection;
 
 use dotenv::dotenv;
 
 mod actor;
 mod auth;
 mod db;
+mod leaderboard;
 mod middleware;
+mod tests;
 use crate::middleware as common_middleware;
+
+#[macro_use]
+extern crate diesel_migrations;
+use diesel_migrations::embed_migrations;
+
+embed_migrations!("migrations/");
+use diesel::prelude::*;
+
+use r2d2::Pool;
+use r2d2_diesel::ConnectionManager;
+
+pub struct TestDbManager {
+    conn_pool: Pool<ConnectionManager<PgConnection>>,
+}
+
+impl TestDbManager {
+    pub fn new() -> Self {
+        let db_url = dotenv::var("TEST_DB_URL").expect("TEST_DB_URL must be set");
+        let conn_manager = ConnectionManager::<PgConnection>::new(db_url);
+        let conn_pool = Pool::builder()
+            .build(conn_manager)
+            .expect("Failed to create test pool");
+
+        let conn = conn_pool.get().unwrap();
+
+        embedded_migrations::run(&*conn).expect("Couldn't run migrations on Test DB");
+
+        Self {
+            conn_pool: conn_pool,
+        }
+    }
+}
+
+impl Drop for TestDbManager {
+    fn drop(&mut self) {
+        let conn = self.conn_pool.get().unwrap();
+
+        let table_list = vec![
+            "events",
+            "regions",
+            "status",
+            "users",
+            "__diesel_schema_migrations",
+        ];
+        let all_tables = table_list.join(", ");
+
+        let query = diesel::sql_query(format!("DROP TABLE {} CASCADE", all_tables).as_str());
+        query
+            .execute(&*conn)
+            .expect("Couldn't delete tables");
+    }
+}
 
 pub async fn ws_index(
     r: HttpRequest,
