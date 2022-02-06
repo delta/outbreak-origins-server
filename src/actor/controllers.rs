e7cff63b02396d211e0886194fd61ca949cd8dc8
KarthikSundar2002 <karthiksundar30092002@gmail.com>
2022-02-06 21:58:41 +0530

    feat: Implements events interaction with simulator

--- src/actor/controllers.rs
+++ src/actor/controllers.rs
@@ -1,11 +1,13 @@
-use crate::actor::events::types::{SimulatorResponse, ControlMeasure, WSResponse, CurParams};
-use crate::actor::types::{ParamsDelta, ControlMeasureParams};
+use crate::actor::events::types::{
+    ControlMeasure, CurParams, Event, SimulatorResponse, WSResponse,
+};
+use crate::actor::types::{ControlMeasureParams, EventParam, ParamsDelta};
+use crate::actor::utils::serialize_state;
 use serde::de::DeserializeOwned;
+use std::collections::HashMap;
+use std::fs;
 use std::path::Path;
 use virus_simulator::Simulator;
-use std::fs;
-use crate::actor::utils::serialize_state;
-use std::collections::HashMap;
 
 const POPULATION: f64 = 10000000.0;
 const TOTAL_DAYS: f64 = 700.0;
@@ -36,6 +38,24 @@ impl RequestType for ControlMeasure {
     }
 }
 
+impl RequestType for Event {
+    fn type_name() -> String {
+        "event".to_string()
+    }
+    fn name(&self) -> String {
+        self.name.clone()
+    }
+    fn params(&self) -> CurParams {
+        self.params.clone()
+    }
+    fn level(&self) -> i32 {
+        self.level
+    }
+    fn cur_date(&self) -> f64 {
+        self.cur_date
+    }
+}
+
 pub trait EventResponseType {
     fn params_delta(&self) -> Vec<ParamsDelta>;
 }
@@ -46,24 +66,29 @@ impl EventResponseType for ControlMeasureParams {
     }
 }
 
-pub fn handle_request<T: RequestType + DeserializeOwned, E: EventResponseType + DeserializeOwned>(
+impl EventResponseType for EventParam {
+    fn params_delta(&self) -> Vec<ParamsDelta> {
+        self.params_delta.clone()
+    }
+}
+
+pub fn handle_request<
+    T: RequestType + DeserializeOwned,
+    E: EventResponseType + DeserializeOwned,
+>(
     payload: String,
 ) -> WSResponse {
     let ws_input = serde_json::from_str::<T>(&payload);
     let res = match ws_input {
         Err(_) => WSResponse::Error("Invalid request sent(Parsing)".to_string()),
         Ok(event) => {
-
             // TODO: Change to be based on user level
             let file = format!("src/game/levels/{}/{}.json", event.level(), T::type_name());
             let path = Path::new(&file);
-            let contents = fs::read_to_string(&path)
-                .expect("Something  went wrong reading the file");
+            let contents =
+                fs::read_to_string(&path).expect("Something  went wrong reading the file");
             let control_measure_params =
-                serde_json::from_str::<HashMap<String, ControlMeasureParams>>(
-                    &contents,
-                )
-                .unwrap();
+                serde_json::from_str::<HashMap<String, ControlMeasureParams>>(&contents).unwrap();
 
             let sent_params = event.params();
             match control_measure_params.get(&event.name()) {
@@ -74,19 +99,16 @@ pub fn handle_request<T: RequestType + DeserializeOwned, E: EventResponseType + 
                         sent_params.recovery_rate,
                         sent_params.infection_rate,
                     ];
-                    let changed_params = params.params_delta.iter().fold(
-                        initial,
-                        |mut acc, x| {
-                            match x.name.as_str() {
-                                "Ideal Reproduction" => acc[0] += x.value,
-                                "Compliance Factor" => acc[1] += x.value,
-                                "Recovery Rate" => acc[2] += x.value,
-                                "Infection Rate" => acc[3] += x.value,
-                                _ => {}
-                            };
-                            acc
-                        },
-                    );
+                    let changed_params = params.params_delta.iter().fold(initial, |mut acc, x| {
+                        match x.name.as_str() {
+                            "Ideal Reproduction" => acc[0] += x.value,
+                            "Compliance Factor" => acc[1] += x.value,
+                            "Recovery Rate" => acc[2] += x.value,
+                            "Infection Rate" => acc[3] += x.value,
+                            _ => {}
+                        };
+                        acc
+                    });
                     let susceptible = sent_params.susceptible / POPULATION;
                     let exposed = sent_params.exposed / POPULATION;
                     let infectious = sent_params.infectious / POPULATION;
@@ -105,13 +127,25 @@ pub fn handle_request<T: RequestType + DeserializeOwned, E: EventResponseType + 
                     );
                     let f = sim.simulate(0_f64, TOTAL_DAYS - cur_date);
                     let payload = serialize_state(&f, POPULATION);
-                    WSResponse::Control(SimulatorResponse {
-                        payload,
-                        ideal_reproduction_number: changed_params[0],
-                        compliance_factor: changed_params[1],
-                        recovery_rate: changed_params[2],
-                        infection_rate: changed_params[3],
-                    })
+                    if T::type_name() == "control" {
+                        WSResponse::Control(SimulatorResponse {
+                            payload,
+                            ideal_reproduction_number: changed_params[0],
+                            compliance_factor: changed_params[1],
+                            recovery_rate: changed_params[2],
+                            infection_rate: changed_params[3],
+                        })
+                    } else if T::type_name() == "event" {
+                        WSResponse::Event(SimulatorResponse {
+                            payload,
+                            ideal_reproduction_number: changed_params[0],
+                            compliance_factor: changed_params[1],
+                            recovery_rate: changed_params[2],
+                            infection_rate: changed_params[3],
+                        })
+                    } else {
+                        WSResponse::Error("Invalid Kind".to_string())
+                    }
                 }
                 None => WSResponse::Error("Invalid request sent".to_string()),
             }
