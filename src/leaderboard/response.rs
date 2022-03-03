0037e6fccdffb67633a0c5daface6d5b8e99bf5e
SanjaiPrabakarraj <sanjai.prabakar@gmail.com>
2022-03-03 23:04:22 +0530

    fix(leaderboard): Authenticate route (#31)

--- src/leaderboard/response.rs
+++ src/leaderboard/response.rs
@@ -1,8 +1,10 @@
 use crate::db::models;
+use crate::leaderboard::response::models::LeaderboardEntry;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct LeaderboardResponse {
     pub status: String,
     pub data: Vec<models::LeaderboardEntry>,
+    pub user_rank: LeaderboardEntry,
 }
