e49a27c883246b89a5ed33877c9e44924dc83143
CraftyChimera <76778532+CraftyChimera@users.noreply.github.com>
2022-03-04 22:14:27 +0530

    feat(NewsReels): Added functionality to fetch data from description.json (#32)
    * feat(NewsReels): Added functionality to fetch data from description.json and send data for NewsReels
     -Implemented Response Messages for Control Measures
     -Implemented Event Responses(Accepted,Rejected or Postponed messages)
     -Impemented Event announcements and Random events
    
    * refactor(newsReels): Cleaned up code and changed variable names to be more meaningful
    
    * refactor(Newsreels): Renamed Event Message to NewsRequest and other minor changes

--- src/db/models.rs
+++ src/db/models.rs
@@ -2,9 +2,10 @@ mod auth;
 mod events;
 mod jwt;
 pub mod status;
-mod user;
+pub mod user;
 
 pub use auth::{ChangePassword, ResendVerification, ResetPasswordEmail, ResetToken, UserVerify};
 pub use events::Event;
 pub use jwt::Claims;
+pub use status::Status;
 pub use user::{LeaderboardEntry, LoginUser, NewUser, RegisterUser, TestUser, User};
