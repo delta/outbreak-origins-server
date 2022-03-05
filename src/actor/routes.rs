067f9919fcc2a230fca598e4e0dfc74e32d0fddc
Shreyas Krishnakumar <shreyaskumar1035@gmail.com>
2022-03-05 16:56:39 +0530

    feat(session): Limit user to 1 concurrent session

--- src/actor/routes.rs
+++ src/actor/routes.rs
@@ -3,6 +3,8 @@ use actix_web::{http::StatusCode, web, Error, HttpRequest, HttpResponse};
 use crate::actor::implementation;
 use crate::auth;
 use crate::db::types::PgPool;
+use crate::db::models::User;
+use diesel::prelude::*;
 
 pub async fn ws_index(
     r: HttpRequest,
@@ -10,10 +12,23 @@ pub async fn ws_index(
     pool: web::Data<PgPool>,
     user: auth::extractors::Authenticated,
 ) -> Result<HttpResponse, Error> {
-    println!("Here");
+    use crate::db::schema::users::dsl::*;
+    
+    let conn = pool.get().expect("Couldn't get DB connection");
+    let auth_user = user.0.as_ref().unwrap();
+    let auth_user = users
+        .filter(email.eq(auth_user.email.clone()))
+        .first::<User>(&*conn).expect("User not found");
+    
     if user.is_none() {
         return Ok(HttpResponse::Ok().status(StatusCode::UNAUTHORIZED).finish());
     }
+    
+    // Don't allow multiple simultaneous connections
+    if auth_user.is_active {
+        return Ok(HttpResponse::Ok().status(StatusCode::FORBIDDEN).finish());
+    }
+    
     let res = implementation::ws::start(implementation::Game::new(pool, user), &r, stream);
     println!("{:?}", res);
     res
