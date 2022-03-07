904cae2d9616bb3d957b5e1a61fea7bbb4e27238
NanthR <61490162+NanthR@users.noreply.github.com>
2022-03-07 18:23:07 +0530

    fix(Events): Add simulator params on event accept (#38)

--- src/actor/utils.rs
+++ src/actor/utils.rs
@@ -1,4 +1,9 @@
+use crate::actor::events::types::SimulatorParams;
+use virus_simulator::Simulator;
 use virus_simulator::State;
+
+const POPULATION: f64 = 5000.0;
+const TOTAL_DAYS: f64 = 700.0;
 
 pub fn serialize_state(s: &[State], population: f64) -> String {
     // serilising the data
@@ -22,4 +27,36 @@ pub fn serialize_state(s: &[State], population: f64) -> String {
         .collect::<Vec<String>>()
         .join(",");
     format!("[{}]", res)
+}
+
+pub fn simulate(
+    params: &SimulatorParams,
+    changed_params: &[f64],
+    cur_date: i32,
+) -> (String, f64, f64, f64, f64) {
+    let susceptible = params.susceptible / POPULATION;
+    let exposed = params.exposed / POPULATION;
+    let infectious = params.infectious / POPULATION;
+    let removed = params.removed / POPULATION;
+
+    let sim = Simulator::new(
+        &susceptible,
+        &exposed,
+        &infectious,
+        &removed,
+        &params.current_reproduction_number,
+        &changed_params[0],
+        &changed_params[1],
+        &changed_params[2],
+        &changed_params[3],
+    );
+
+    let f = sim.simulate(0_f64, TOTAL_DAYS - cur_date as f64);
+    (
+        serialize_state(&f, POPULATION),
+        susceptible,
+        exposed,
+        infectious,
+        removed,
+    )
 }
