66f8265913083755dc293ee89a79f3919c5cc2db
Shreyas Krishnakumar <shreyaskumar1035@gmail.com>
2022-03-08 15:47:30 +0530

    refactor(levels): Remove unused route start-level

--- src/game/routes.rs
+++ src/game/routes.rs
@@ -2,44 +2,10 @@ use crate::auth::extractors::Authenticated;
 use crate::db::types::PgPool;
 use crate::game::controllers::get_active_control_measures;
 use crate::game::response;
-use crate::levels::controllers::{get_current_level, update_user_at_level_end};
+use crate::levels::controllers::update_user_at_level_end;
 use actix_web::{get, post, web, Error, HttpResponse};
 use std::collections::HashMap;
-use std::fs::File;
-use std::io::Read;
 use tracing::{instrument, error, info};
-
-#[get("/start-level")]
-#[instrument(skip(pool))]
-async fn start_level(
-    user: Authenticated,
-    pool: web::Data<PgPool>,
-    level: web::Query<response::StartLevelRequest>,
-) -> Result<HttpResponse, Error> {
-    let cur_level = get_current_level(&pool.get().unwrap(), user);
-    if cur_level < level.level {
-        Ok(HttpResponse::Ok().json(response::StartLevelError {
-            message: format!("Level {} is not yet unlocked", level.level),
-        }))
-    } else {
-        let mut file =
-            match File::open(format!("src/game/levels/{}/level_start.json", level.level)) {
-                Ok(start_file) => start_file,
-                Err(e) => {
-                    error!("Couldn't open level_start.json: {}", e);
-                    File::open(format!("src/game/levels/{}/level_start.json", cur_level)).unwrap()
-                }
-            };
-        let mut json_string = String::new();
-        file.read_to_string(&mut json_string).unwrap_or_else(|e| {
-            error!("Couldn't read level_start.json: {}", e);
-            0
-        });
-        Ok(HttpResponse::Ok()
-            .content_type("application/json")
-            .body(json_string))
-    }
-}
 
 #[get("/active-control-measures")]
 #[instrument(skip(pool))]
@@ -104,7 +70,6 @@ async fn end_level(
 pub fn game_routes(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/user/api/")
-            .service(start_level)
             .service(active_control_measures)
             .service(end_level),
     );
--- src/levels/controllers.rs
+++ src/levels/controllers.rs
@@ -27,6 +27,7 @@ pub fn update_user_at_level_end(
     }
 }
 
+#[allow(dead_code)]
 pub fn get_current_level(conn: &PgConnection, user: Authenticated) -> i32 {
     use crate::db::schema::users::dsl::*;
     let user_email = user.0.as_ref().map(|y| y.email.clone());
