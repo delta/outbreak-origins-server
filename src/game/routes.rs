cb58db7c29f090b8d2ed90b7f8bcab5ba564aee7
Shreyas K <63252528+Shreyas-21@users.noreply.github.com>
2022-03-08 20:12:03 +0530

    feat(app): Add log messages and refactor routes (#39)
    * feat(logging): Initialize logging configuration
    
    * feat(logging): Add log messages and remove printlns
    
    * refactor(levels): Remove unused route start-level
    
    * refactor(game): Move update_user_at_level_end to game controllers
    
    * fix(ranking): Get user rank over all users
    
    * fix(levels): Remove only levelDetails.json
    
    * fix(lint): Clippy errors
    
    * fix(logging): Write logs to a file

--- src/game/routes.rs
+++ src/game/routes.rs
@@ -1,42 +1,20 @@
 use crate::auth::extractors::Authenticated;
 use crate::db::types::PgPool;
 use crate::game::controllers::get_active_control_measures;
+use crate::game::controllers::update_user_at_level_end;
 use crate::game::response;
-use crate::levels::controllers::{get_current_level, update_user_at_level_end};
 use actix_web::{get, post, web, Error, HttpResponse};
 use std::collections::HashMap;
-use std::fs::File;
-use std::io::Read;
-
-#[get("/start-level")]
-async fn start_level(
-    user: Authenticated,
-    pool: web::Data<PgPool>,
-    level: web::Query<response::StartLevelRequest>,
-) -> Result<HttpResponse, Error> {
-    let cur_level = get_current_level(&pool.get().unwrap(), user);
-    if cur_level < level.level {
-        Ok(HttpResponse::Ok().json(response::StartLevelError {
-            message: format!("Level {} is not yet unlocked", level.level),
-        }))
-    } else {
-        let mut file =
-            File::open(format!("src/game/levels/{}/level_start.json", level.level)).unwrap();
-        let mut json_string = String::new();
-        file.read_to_string(&mut json_string).unwrap();
-        Ok(HttpResponse::Ok()
-            .content_type("application/json")
-            .body(json_string))
-    }
-}
+use tracing::{error, info, instrument};
 
 #[get("/active-control-measures")]
+#[instrument(skip(pool))]
 async fn active_control_measures(
     user: Authenticated,
     pool: web::Data<PgPool>,
 ) -> Result<HttpResponse, Error> {
     let acm_res = get_active_control_measures(&pool.get().unwrap(), user).map_err(|e| {
-        eprintln!("{}", e);
+        error!("Couldn't get active control measures: {}", e);
         HttpResponse::InternalServerError().json(response::ActiveControlMeasuresResponse {
             active_control_measures: HashMap::new(),
         })
@@ -45,6 +23,7 @@ async fn active_control_measures(
 }
 
 #[post("/end-level")]
+#[instrument(skip(pool))]
 async fn end_level(
     user: Authenticated,
     pool: web::Data<PgPool>,
@@ -68,24 +47,29 @@ async fn end_level(
 
     let score = score_scale * (10.0 + performance_factor);
 
-    match update_user_at_level_end(&pool.get().unwrap(), user, score as i32) {
-        Ok(_) => Ok(HttpResponse::Ok().json(response::EndLevelResponse {
-            message: "Success".to_string(),
-            score,
-        })),
-        Err(_) => Ok(
-            HttpResponse::InternalServerError().json(response::EndLevelResponse {
-                message: "Failed".to_string(),
-                score: 0.0,
-            }),
-        ),
+    match update_user_at_level_end(&pool.get().unwrap(), user) {
+        Ok(_) => {
+            info!("User ended level successfully");
+            Ok(HttpResponse::Ok().json(response::EndLevelResponse {
+                message: "Success".to_string(),
+                score,
+            }))
+        }
+        Err(e) => {
+            error!("Couldn't update user: {}", e);
+            Ok(
+                HttpResponse::InternalServerError().json(response::EndLevelResponse {
+                    message: "Failed".to_string(),
+                    score: 0.0,
+                }),
+            )
+        }
     }
 }
 
 pub fn game_routes(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/user/api/")
-            .service(start_level)
             .service(active_control_measures)
             .service(end_level),
     );
--- src/levels.rs
+++ src/levels.rs
@@ -1,3 +1,3 @@
 pub mod controllers;
-mod response;
+pub mod response;
 pub mod routes;
