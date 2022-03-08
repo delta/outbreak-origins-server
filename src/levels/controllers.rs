9875e19c6741a4922c5735ed7076f085fc667a65
Shreyas Krishnakumar <shreyaskumar1035@gmail.com>
2022-03-08 18:38:37 +0530

    fix(levels): Remove only levelDetails.json

--- src/levels/controllers.rs
+++ src/levels/controllers.rs
@@ -4,7 +4,6 @@ use crate::db::types::DbError;
 use crate::levels::response;
 use diesel::prelude::*;
 
-#[allow(dead_code)]
 pub fn get_current_level(conn: &PgConnection, user: Authenticated) -> i32 {
     use crate::db::schema::users::dsl::*;
     let user_email = user.0.as_ref().map(|y| y.email.clone());
