8b1b0008c3d7bdda815293e302501db415b15e7c
KarthikSundar2002 <karthiksundar30092002@gmail.com>
2022-03-08 23:44:21 +0530

    feat(game): Limit user to 3 tries per level

--- src/levels/routes.rs
+++ src/levels/routes.rs
@@ -8,7 +8,11 @@ async fn level_details(
     user: Authenticated,
     pool: web::Data<PgPool>,
 ) -> Result<HttpResponse, Error> {
-    let curr_level = controllers::get_current_level(&pool.get().unwrap(), user);
+    let mut curr_level = controllers::get_current_level(&pool.get().unwrap(), &user);
+    let retryattemptsleft = controllers::get_retry_attempts(&pool.get().unwrap(), &user);
+    if retryattemptsleft < 1 {
+        curr_level = -1;
+    }
     Ok(HttpResponse::Ok().json(response::LevelResponse {
         cur_level: curr_level,
     }))
